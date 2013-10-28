b88a2d0cd280852c137b5fa05a42b4055893488d
Tim Hartmann <tfhartmann@gmail.com>
2013-10-28 18:08:02 -0400

    Added different OS's that work

--- spec/classes/common_spec.rb
+++ spec/classes/common_spec.rb
@@ -1,13 +1,37 @@
 require 'spec_helper'
 
 describe 'common', :type => :class do
-
-  describe "HUIT Common class with no parameters, basic test" do
+  context "HUIT Common class with no parameters, basic test - RedHat" do
     let(:params) { { } }
-    
-      it {
-        should include_class('common::files')
-        should include_class('common::packages')
-      }
+    let :facts do {
+        :operatingsystem => 'RedHat'
+    } 
+    end
+    it {
+      should include_class('common::files')
+      should include_class('common::packages')
+    }
+  end
+  context "HUIT Common class with no parameters, basic test - CentOS" do
+    let(:params) { { } }
+    let :facts do {
+        :operatingsystem => 'CentOS'
+    } 
+    end
+    it {
+      should include_class('common::files')
+      should include_class('common::packages')
+    }
+  end
+  context "HUIT Common class with no parameters, basic test - Amazon Linux" do
+    let(:params) { { } }
+    let :facts do {
+        :operatingsystem => 'Amazon'
+    } 
+    end
+    it {
+      should include_class('common::files')
+      should include_class('common::packages')
+    }
   end
 end
