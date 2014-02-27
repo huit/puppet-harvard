8dbe11418b41d962228b18daeed5b6244e5f34de
Tim Hartmann <tfhartmann@gmail.com>
2014-02-27 13:58:12 -0800

    Er - adding tests..

---
+++ spec/classes/harvard_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe 'harvard', :type => :class do
+  context "Harvard class with no parameters, basic test - RedHat" do
+    let(:params) { { } }
+    let :facts do {
+        :operatingsystem => 'RedHat'
+    } 
+    end
+    it {
+      should include_class('harvard::files')
+      should include_class('harvard::packages')
+    }
+  end
+  context "Harvard Common class with no parameters, basic test - CentOS" do
+    let(:params) { { } }
+    let :facts do {
+        :operatingsystem => 'CentOS'
+    } 
+    end
+    it {
+      should include_class('harvard::files')
+      should include_class('harvard::packages')
+    }
+  end
+  context "Harvard class with no parameters, basic test - Amazon Linux" do
+    let(:params) { { } }
+    let :facts do {
+        :operatingsystem => 'Amazon'
+    } 
+    end
+    it {
+      should include_class('harvard::files')
+      should include_class('harvard::packages')
+    }
+  end
+  context "Harvard class with no parameters, basic test - Unsupported" do
+    let(:params) { { } }
+    it do 
+      expect {
+      should include_class('harvard::files')
+      should include_class('harvard::packages')
+    }.to raise_error(Puppet::Error, /is not supported/)
+    end
+  end
+end
