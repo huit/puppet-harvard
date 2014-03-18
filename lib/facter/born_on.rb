ae3ddaaf98db551f06a863329187088948eb4788
Tim Hartmann <tfhartmann@gmail.com>
2014-03-18 13:55:43 -0700

    Adding @sjoeboo born_on fact
    Adding the born_on fact written by @sjoeboo to the harvard module
    This is work towards issue #1

---
+++ lib/facter/born_on.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby
+require 'facter'
+
+begin
+          Facter.operatingsystem
+rescue
+          Facter.loadfacts()
+end
+os = Facter.value("operatingsystem")
+if os.match(/CentOS|RedHat/) then
+  unless  `rpm -q --last basesystem`.empty?
+      Facter.add("born_on") do
+          setcode do
+              date = `rpm -q --qf '%{INSTALLTIME}' basesystem`
+              born_on = `date --date=@#{date} +%F`.chomp
+              born_on
+          end
+      end
+  end
+end
+
