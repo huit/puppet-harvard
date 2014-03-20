76fa64081a9a1705844c926d5cf783a86e2cbf8a
Steve Huff <shuff@vecna.org>
2014-03-20 10:14:04 -0400

    omg the tabs are enormous

--- lib/facter/born_on.rb
+++ lib/facter/born_on.rb
@@ -2,20 +2,20 @@
 require 'facter'
 
 begin
-          Facter.operatingsystem
+  Facter.operatingsystem
 rescue
-          Facter.loadfacts()
+  Facter.loadfacts()
 end
 os = Facter.value("operatingsystem")
 if os.match(/CentOS|RedHat/) then
   unless  `rpm -q --last basesystem`.empty?
-      Facter.add("born_on") do
-          setcode do
-              date = `rpm -q --qf '%{INSTALLTIME}' basesystem`
-              born_on = `date --date=@#{date} +%F`.chomp
-              born_on
-          end
+    Facter.add("born_on") do
+      setcode do
+        date = `rpm -q --qf '%{INSTALLTIME}' basesystem`
+        born_on = `date --date=@#{date} +%F`.chomp
+        born_on
       end
+    end
   end
 end
 
